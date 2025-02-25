f17f8b003eb2c3bb1a32d421463bb817e1911d58
kimthoa <kiimthoa>
2025-02-25 15:27:47 +0700

    Hien thi danh sach cac contact lay ve tu server

---
+++ src/components/InputSearch.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+    props: {
+        modelValue: { type: String, default: "" }, //prop nhận dữ liệu từ component cha (dùng để binding giá trị ô nhập liệu).
+    },
+    emits: ["submit", "update:modelValue"], //sự kiện submit và sự kiện update
+    methods: {
+        updateModelValue(e) {
+            this.$emit("update:modelValue", e.target.value);
+        },
+        submit() {
+            this.$emit("submit");
+        },
+    },
+};
+</script>
+<template>
+    <div class="input-group">
+        <input type="text" class="form-control" placeholder="Nhập thông tin cần tìm" :value="modelValue"
+            @input="updateModelValue" @keyup.enter="submit" />
+        <div class="input-group-append">
+            <button class="btn btn-outline-secondary" type="button" @click="submit">
+                <i class="fas fa-search"></i> Tìm kiếm
+            </button>
+        </div>
+    </div>
+</template>F
